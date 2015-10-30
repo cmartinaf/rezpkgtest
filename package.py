d909fc9b5f9500452a937bb476bd3c47b36bd858
Chris Martin <cmartin@atomicfiction.com>
2015-10-30 14:38:14 -0400

    adding

---
+++ package.py
@@ -0,0 +1,25 @@
+name = 'dpoolmaya'
+
+version = '1.0.0'
+
+description = \
+    """
+    Maya package for dpool.
+    """
+
+build_requires = [
+    "python-2.7",
+]
+
+variants = [
+    ["platform-linux", "python-2.7"]
+]
+requires = [
+    "python-2.7"
+]
+
+uuid = "dpoolmaya"
+
+def commands():
+    pass
+
